de530880aa3f5ec7573e48bba30dad50b6787c5d
ninsuo <ninsuo@gmail.com>
2015-02-03 17:03:58 +0100

    empty bundles (app + quickstart)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new Fuz\QuickStartBundle\FuzQuickStartBundle(),
+            new Fuz\AppBundle\FuzAppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/Fuz/AppBundle/FuzAppBundle.php
+++ src/Fuz/AppBundle/FuzAppBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace Fuz\AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class FuzAppBundle extends Bundle
 {
 }
