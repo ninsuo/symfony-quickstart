6167e557cd8db6b4cf79023f82957923ce4cf12c
alain <ninsuo@gmail.com>
2015-02-17 19:50:19 +0100

    removed projectbundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,7 +25,6 @@ class AppKernel extends Kernel
                 new SmartCore\Bundle\AcceleratorCacheBundle\AcceleratorCacheBundle(),
                 new Fuz\QuickStartBundle\FuzQuickStartBundle(),
                 new Fuz\AppBundle\FuzAppBundle(),
-            new Fuz\ProjectBundle\FuzProjectBundle(),
         );
 
         if (in_array($this->getEnvironment(), array ('dev', 'test')))
--- src/Fuz/ProjectBundle/FuzProjectBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Fuz\ProjectBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class FuzProjectBundle extends Bundle
-{
-}
