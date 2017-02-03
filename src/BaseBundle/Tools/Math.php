8c9b5765d6c0270647fd30d37ed0a8dc2668bdac
Alain Tiemblo <ninsuo@gmail.com>
2017-02-03 16:58:34 +0100

    Update Math.php

--- src/BaseBundle/Tools/Math.php
+++ src/BaseBundle/Tools/Math.php
@@ -4,13 +4,24 @@ namespace BaseBundle\Tools;
 
 class Math
 {
-    public static function rand($size = 16)
+    public function rand($expected = 10)
     {
-        $string = '';
-        while (strlen($string) < $size) {
-            $string .= base_convert(sha1(microtime(true).uniqid(mt_rand(), true)), 16, 36);
+        $allowed = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890';
+        $handle = fopen('/dev/urandom', 'r');
+        $current = 0;
+        $result = '';
+        while ($current < $expected) {
+            $chars = fread($handle, 1024);
+            for ($i = 0; $i < 1024; $i++) {
+                $char = ord($chars[$i]);
+                if (strpos($allowed, $char)) {
+                    $result .= $char;
+                    $current++;
+                }
+            }
         }
+        fclose($handle);
 
-        return substr($string, 0, $size);
+        return $result;
     }
 }
