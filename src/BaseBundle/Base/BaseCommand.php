4f1f4279a5f1134f9ba790c29179d809029cd0cb
Alain Tiemblo <ninsuo@gmail.com>
2017-06-18 18:26:39 +0200

    fix BaseCommand so it has the container compatible with base service

--- src/BaseBundle/Base/BaseCommand.php
+++ src/BaseBundle/Base/BaseCommand.php
@@ -3,9 +3,12 @@
 namespace BaseBundle\Base;
 
 use BaseBundle\Traits\ServiceTrait;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
-abstract class BaseCommand extends ContainerAwareCommand
+abstract class BaseCommand extends Command implements ContainerAwareInterface
 {
     use ServiceTrait;
+    use ContainerAwareTrait;
 }
