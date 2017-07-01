27389d82d8e5c24b24f38112b04833bd025e53bb
alain tiemblo <ninsuo@gmail.com>
2017-07-01 19:04:47 +0200

    removed setting entity

--- src/BaseBundle/Twig/Extension/LightExtension.php
+++ src/BaseBundle/Twig/Extension/LightExtension.php
@@ -10,32 +10,16 @@ class LightExtension extends BaseTwigExtension
     {
         return [
             new \Twig_SimpleFunction('http_build_query', 'http_build_query', ['is_safe' => ['html', 'html_attr']]),
-            new \Twig_SimpleFunction('array_to_query_fields', [$this, 'arrayToQueryFields'], ['is_safe' => ['html']]),
             new \Twig_SimpleFunction('absolute_url', [$this, 'absoluteUrl']),
             new \Twig_SimpleFunction('current_route', [$this, 'currentRoute']),
             new \Twig_SimpleFunction('current_locale', [$this, 'currentLocale']),
             new \Twig_SimpleFunction('current_uri', [$this, 'currentUri']),
             new \Twig_SimpleFunction('page_id', [$this, 'pageId']),
+            new \Twig_SimpleFunction('array_to_query_fields', [$this, 'arrayToQueryFields'], ['is_safe' => ['html']]),
         ];
     }
 
-    public function arrayToQueryFields($key, $value, $keyPrefix = null)
-    {
-        $currentKey = $keyPrefix ? $keyPrefix.'['.$key.']' : $key;
-
-        if (is_string($value)) {
-            return '<input type="hidden" name="'.htmlentities($currentKey).'" value="'.htmlentities($value).'"/>';
-        }
-
-        $inputs = '';
-        foreach ($value as $childKey => $childValue) {
-            $inputs .= $this->arrayToQueryFields($childKey, $childValue, $currentKey);
-        }
-
-        return $inputs;
-    }
-
-    public function absoluteUrl($asset)
+    public function absoluteUrl($asset = '')
     {
         $request = $this->get('request_stack')->getMasterRequest();
         $baseurl = $request->getScheme().'://'.$request->getHttpHost().$request->getBasePath();
@@ -76,6 +60,31 @@ class LightExtension extends BaseTwigExtension
         }
 
         return $data;
+    }
+
+    /**
+     * Used to keep arguments of the query string when generating a new form with method GET.
+     * See: BaseBundle::macros.html.twig
+     *
+     * @param string $key
+     * @param mixed $value
+     * @param string $keyPrefix
+     * @return string
+     */
+    public function arrayToQueryFields($key, $value, $keyPrefix = null)
+    {
+        $currentKey = $keyPrefix ? $keyPrefix.'['.$key.']' : $key;
+
+        if (is_string($value)) {
+            return '<input type="hidden" name="'.htmlentities($currentKey).'" value="'.htmlentities($value).'"/>';
+        }
+
+        $inputs = '';
+        foreach ($value as $childKey => $childValue) {
+            $inputs .= $this->arrayToQueryFields($childKey, $childValue, $currentKey);
+        }
+
+        return $inputs;
     }
 
     public function getName()
